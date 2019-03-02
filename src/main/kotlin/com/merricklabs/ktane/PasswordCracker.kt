0a288ed78876d58fa5cdf5dff9544858ee8e98f4
David Merrick <david@david-merrick.com>
2019-03-01 19:24:40 -0800

    Fixed bug with backtracking. Added tests.

--- src/main/kotlin/com/merricklabs/ktane/PasswordCracker.kt
+++ src/main/kotlin/com/merricklabs/ktane/PasswordCracker.kt
@@ -2,15 +2,7 @@ package com.merricklabs.ktane
 
 import java.util.Stack
 
-class PasswordCracker {
-
-    val columns = listOf(
-            "dcwpjz",
-            "niexrg",
-            "nidsbz",
-            "xxxtxx",
-            "xxexxx"
-    )
+data class PasswordCracker(val columns: List<String>) {
 
     val words = listOf(
             "about", "after", "again", "below", "could",
@@ -21,61 +13,69 @@ class PasswordCracker {
             "these", "thing", "think", "three", "water",
             "where", "which", "world", "would", "write"
     )
-}
 
-fun crackPassword(): String {
-    val cracker = PasswordCracker()
+    fun crackPassword(): String {
+        // Build a Trie out of the valid words
+        val trie = Trie()
+        words.forEach { trie.insert(it) }
 
-    // Build a Trie out of the valid words
-    val trie = Trie()
-    cracker.words.forEach { trie.insert(it) }
+        // Perform a depth-first search of the tree
+        val visited = mutableListOf<Node>()
+        var node = trie.root
 
-    // Perform a depth-first search of the tree
-    val visited = mutableListOf<Node>()
-    var node = trie.root
-
-    val stack = Stack<Node>()
-    stack.push(node)
-    var columnIndex = 0
-    while(stack.size < cracker.columns.size + 1){ // +1 to account for root node
-        // Have we visited everything here?
-        if(shouldBacktrack(node, cracker.columns[columnIndex], visited)){
-            node = stack.pop()
-            check(!node.isRoot())
-            columnIndex--
-        } else {
-            // Visit stuff here
-            val validChars = cracker.columns[columnIndex].toSet().intersect(node.children.keys).toList()
-            for(i in 0 until validChars.size){
-                val child = node.children[validChars[i]]
-                if(!visited.contains(child)){
-                    node = child!!
-                    columnIndex++
-                    stack.push(node)
-                    visited.add(node)
-                    break
+        val stack = Stack<Node>()
+        stack.push(node)
+        var columnIndex = 0
+        while(stack.size < columns.size + 1){ // +1 to account for root node
+            // Have we visited everything here?
+            if(shouldBacktrack(node, columns[columnIndex], visited)){
+                stack.pop()
+                node = stack.peek()
+                columnIndex--
+                check(columnIndex >= 0)
+            } else {
+                // Visit stuff here
+                val validChars = columns[columnIndex].toSet().intersect(node.children.keys).toList()
+                for(i in 0 until validChars.size){
+                    val child = node.children[validChars[i]]
+                    if(!visited.contains(child)){
+                        node = child!!
+                        columnIndex++
+                        check(columnIndex <= columns.size)
+                        stack.push(node)
+                        visited.add(node)
+                        break
+                    }
                 }
             }
         }
+
+        return stack.asSequence().filter { it.value != trie.root.value }
+                .map { it.value.toString() }
+                .reduce { a, b -> "$a$b" }
     }
 
-    return stack.asSequence().filter { it.value != trie.root.value }
-            .map { it.value.toString() }
-            .reduce { a, b -> "$a$b" }
+    fun shouldBacktrack(node: Node, column: String, visited: List<Node>): Boolean {
+        // If we've visited all the nodes, or if none of the nodes are in the column, backtrack
+        val keySet = node.children.keys
+        val columnSet = column.toSet()
+        val noValidNodes = keySet.intersect(columnSet).isEmpty()
+
+        val visitedSet = visited.toSet()
+        val nodeSet = node.children.values.toSet()
+        val allVisited = visitedSet.intersect(nodeSet) == nodeSet
+        return noValidNodes || allVisited
+    }
 }
 
 fun main(args: Array<String>) {
-    println("Solution: ${crackPassword()}")
-}
-
-fun shouldBacktrack(node: Node, column: String, visited: List<Node>): Boolean {
-    // If we've visited all the nodes, or if none of the nodes are in the column, backtrack
-    val keySet = node.children.keys
-    val columnSet = column.toSet()
-    val noValidNodes = keySet.intersect(columnSet).isEmpty()
-
-    val visitedSet = visited.toSet()
-    val nodeSet = node.children.values.toSet()
-    val allVisited = visitedSet.intersect(nodeSet) == nodeSet
-    return noValidNodes || allVisited
+    val columns = listOf(
+            "dcwpjz",
+            "niexrg",
+            "nidsbz",
+            "xxxtxx",
+            "xxexxx"
+    )
+    val cracker = PasswordCracker(columns)
+    println("Solution: ${cracker.crackPassword()}")
 }
--- src/test/kotlin/com/merricklabs/ktane/PasswordCrackerTest.kt
+++ src/test/kotlin/com/merricklabs/ktane/PasswordCrackerTest.kt
@@ -1,5 +1,6 @@
 package com.merricklabs.ktane
 
+import org.testng.Assert
 import org.testng.annotations.Test
 
 class PasswordCrackerTest {
@@ -12,6 +13,24 @@ class PasswordCrackerTest {
                 "nidsbz"
         )
 
-        // Answer: write
+        val cracker = PasswordCracker(columns)
+        val solution = cracker.crackPassword()
+        val candidates = cracker.words.find { it.startsWith(solution) }
+        Assert.assertTrue(candidates!!.contains("write"))
+    }
+
+    @Test
+    fun `Five column test`(){
+        val columns = listOf(
+                "dcwpjz",
+                "niexrg",
+                "nidsbz",
+                "xxxtxx",
+                "xxexxx"
+        )
+
+        val cracker = PasswordCracker(columns)
+        val solution = cracker.crackPassword()
+        Assert.assertEquals(solution, "write")
     }
 }
